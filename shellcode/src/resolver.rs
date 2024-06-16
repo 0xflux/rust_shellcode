1610935352ae6158073f551360a86b536528c9a6
flux <49762827+0xflux@users.noreply.github.com>
2024-06-16 08:00:06 +0100

    big updates

--- shellcode/src/resolver.rs
+++ shellcode/src/resolver.rs
@@ -1,28 +1,10 @@
-#![allow(non_snake_case)]
-#![allow(non_camel_case_types)]
-#![allow(dead_code)]
+use core::{arch::asm, ops::Add};
 
-use core::{arch::asm, ffi::c_void, ops::Add, str::{from_utf8, from_utf8_mut}};
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+#[link(name = "vcruntime")] extern {}
 
-/// A structure containing the module name, function name, and export address for each function loaded
-/// into the portable executable on x64 systems only.
-pub struct ExportResolver<'a> {
-    pub module: &'a str,
-    pub base_address: usize, // to prevent repeat reads of the peb
-    pub function: &'a str,
-    pub address: usize,
-}
-
-/// Get the base address of a specified module. Obtains the base address by reading from the TEB -> PEB -> 
-/// PEB_LDR_DATA -> InMemoryOrderModuleList -> InMemoryOrderLinks -> DllBase 
-/// 
-/// Returns the DLL base address as a Option<usize> 
-#[allow(unused_variables)]
-#[allow(unused_assignments)]
-fn get_module_base(module_name: &[u8]) -> Option<usize> {
-
-    let module_name: &str = from_utf8(module_name).unwrap();
-
+pub extern "system" fn get_dll_base(module_name: *const u16) -> *const c_void {
     let mut peb: usize;
     let mut ldr: usize;
     let mut in_memory_order_module_list: usize;
@@ -48,26 +30,13 @@ fn get_module_base(module_name: &[u8]) -> Option<usize> {
             let dll_base = *(current_entry.add(0x30) as *const usize);
             let module_name_address = *(current_entry.add(0x60) as *const usize);
             let module_length = *(current_entry.add(0x58) as *const u16);
-
+            
             // check if the module name address is valid and not zero
             if module_name_address != 0 && module_length > 0 {
-                // read the module name from memory
-                let mut buffer = [0u8; 512];
-                let dll_name = unsafe {
-                    let mut len = 0;
-                    while len < (module_length / 2) as usize && (module_name_address as *const u16).add(len).read() != 0 {
-                        buffer[len] = (module_name_address as *const u16).add(len).read() as u8;
-                        len += 1;
-                    }
-                    from_utf8(&buffer[..len]).unwrap_or("")
-                };
-
-                // do we have a match on the module name?
-                if dll_name.eq_ignore_ascii_case(module_name) {
-                    return Some(dll_base);
+                let cur_name = core::slice::from_raw_parts(module_name_address as *const u16, (module_length / 2) as usize) as *const _ as *const u16;
+                if raw_eq(module_name, cur_name) {
+                    return dll_base as PVOID;
                 }
-            } else {
-                return None;
             }
 
             // dereference current_entry which contains the value of the next LDR_DATA_TABLE_ENTRY (specifically a pointer to LIST_ENTRY 
@@ -76,262 +45,265 @@ fn get_module_base(module_name: &[u8]) -> Option<usize> {
 
             // If we have looped back to the start, break
             if current_entry == in_memory_order_module_list {
-                return None;
+                // using 12345678 as a marker for it gone wrong
+                return 0x12345678 as *const c_void;
             }
         }
     }
 }
 
-/// Get the function address of a function in a specified DLL from the DLL Base.
-/// 
-/// # Parameters 
-/// * dll_name -> the name of the DLL / module you are wanting to query
-/// * needle -> the function name (case sensitive) of the function you are looking for
-/// 
-/// # Returns
-/// Option<*const c_void> -> the function address as a pointer
-pub fn get_function_from_exports<'a>(dll_name: &'a [u8], needle: &'a [u8]) -> Option<ExportResolver<'a>> {
+/// Get the function address from the givin module DLL base
+pub extern "system" fn get_function_from_exports(dll_base: *const c_void, needle: *const u8) -> *const c_void {
+    unsafe {
+        // cast the dos header
+        let dos_header = &*(dll_base as *const IMAGE_DOS_HEADER);
+        if (*dos_header).e_magic != IMAGE_DOS_SIGNATURE {
+            return 0x12345678 as *const c_void;
+        }
 
-    let dll_name = strip_null_terminator(dll_name);
-    let needle = strip_null_terminator(&needle);
+        // get us to the export directory
+        let e_lfanew = (*dos_header).e_lfanew;
+        let nt_headers = (dll_base as *const u8).offset(e_lfanew as isize) as *const IMAGE_NT_HEADERS64;
+        let optional_headers = &(*nt_headers).OptionalHeader;
+        let virtual_addr = (&optional_headers.DataDirectory[0]).VirtualAddress;
+        let export_dir: *const IMAGE_EXPORT_DIRECTORY = (dll_base as *const u8).offset(virtual_addr as _) as _;
 
-    let needle = from_utf8(&needle).unwrap();
+        // get the location of each list
+        let number_of_names = (*export_dir).NumberOfNames;
+        let addr_of_funcs = (*export_dir).AddressOfFunctions;
+        let addr_of_names = (*export_dir).AddressOfNames;
+        let addr_of_ords = (*export_dir).AddressOfNameOrdinals;
 
-    // if the dll_base was already found from a previous search then use that
-    // otherwise, if it was None, make a call to get_module_base
-    let dll_base: *mut c_void = match get_module_base(dll_name) {
-            Some(a) => a as *mut c_void,
-            None => {
-                return None;
-            },
-        };
+        // iterate through each export, looking for our chosen
+        for i in 0..number_of_names {
+            let name_rva_p: *const DWORD = (dll_base as *const u8).offset((addr_of_names + i * 4) as isize) as *const _;
+            let name_index_p: *const WORD = (dll_base as *const u8).offset((addr_of_ords + i * 2) as isize) as *const _;
+            let name_index = name_index_p.as_ref().unwrap();
+            let mut off: u32 = (4 * name_index) as u32;
+            off = off + addr_of_funcs;
+            let func_rva: *const DWORD = (dll_base as *const u8).offset(off as _) as *const _;
 
-    let dll_name = from_utf8(dll_name).unwrap();
+            let name_rva = name_rva_p.as_ref().unwrap();
+            let curr_name = (dll_base as *const u8).offset(*name_rva as isize);
 
-    // check we match the DOS header, cast as pointer to tell the compiler to treat the memory
-    // address as if it were a IMAGE_DOS_HEADER structure
-    let dos_header: IMAGE_DOS_HEADER = unsafe { read_memory(dll_base as *const IMAGE_DOS_HEADER) };
-    if dos_header.e_magic != IMAGE_DOS_SIGNATURE {
-        return None;
+            if *curr_name == 0 {
+                continue;
+            }
+            if raw_eq(needle, curr_name) {
+                let res = (dll_base as *const u8).offset(*func_rva as isize);
+                return res as *const c_void;
+            }
+        }
     }
 
-    // check the NT headers
-    let nt_headers = unsafe { read_memory(dll_base.offset(dos_header.e_lfanew as isize) as *const IMAGE_NT_HEADERS64) };
-    if nt_headers.Signature != IMAGE_NT_SIGNATURE {
-        return None;
-    }
+    0x11223344 as *const c_void
+}
 
-    // get the export directory
-    // https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-image_data_directory
-    // found from first item in the DataDirectory; then we take the structure in memory at dll_base + RVA
-    let export_dir_rva = nt_headers.OptionalHeader.DataDirectory[0].VirtualAddress;
-    let export_offset = unsafe {dll_base.add(export_dir_rva as usize) };
-    let export_dir: IMAGE_EXPORT_DIRECTORY = unsafe { read_memory(export_offset as *const IMAGE_EXPORT_DIRECTORY) };
-    
-    // get the addresses we need
-    let address_of_functions_rva = export_dir.AddressOfFunctions as usize;
-    let address_of_names_rva = export_dir.AddressOfNames as usize;
-    let ordinals_rva = export_dir.AddressOfNameOrdinals as usize;
+pub fn raw_eq<T>(s: *const T, u: *const T) -> bool
+where
+    T: PartialEq + Default,
+{
+    unsafe {
+        // calculate the length of the first string (s)
+        let s_len = (0..).take_while(|&i| *s.offset(i) != T::default()).count();
+        let s_slice = core::slice::from_raw_parts(s, s_len);
 
-    let functions = unsafe { dll_base.add(address_of_functions_rva as usize) } as *const u32;
-    let names = unsafe { dll_base.add(address_of_names_rva as usize) } as *const u32;
-    let ordinals = unsafe { dll_base.add(ordinals_rva as usize) } as *const u16;
+        // calculate the length of the second string (u)
+        let u_len = (0..).take_while(|&i| *u.offset(i) != T::default()).count();
+        let u_slice = core::slice::from_raw_parts(u, u_len);
 
-    // get the amount of names to iterate over
-    let number_of_names = export_dir.NumberOfNames;
+        // compare lengths
+        if s_len != u_len {
+            return false;
+        }       
 
-    for i in 0..number_of_names {
-        // calculate the RVA of the function name
-        let name_rva = unsafe { *names.offset(i.try_into().unwrap()) as usize };
-        // actual memory address of the function name
-        let name_addr = unsafe { dll_base.add(name_rva) };
-        
-        // read the function name
-        let mut function_name = [0u8; 256];
-        let mut len = 0;
-        unsafe {
-            while len < function_name.len() && (name_addr.add(len) as *const u8).read() != 0 {
-                function_name[len] = (name_addr.add(len) as *const u8).read();
-                len += 1;
+        // compare content
+        for i in 0..s_len {
+            // bounds check on u; shouldn't matter actually, but leaving for safety
+            if i > u_len {
+                return false;
+            }
+
+            // compare each char in the string
+            if s_slice[i] != u_slice[i] {
+                return false;
             }
         }
 
-        let function_name = from_utf8(&function_name[..len]).unwrap_or("Invalid UTF-8");
-        if function_name.eq("Invalid UTF-8") {
-            return None;
-        }
-
-        // if we have a match on our function name
-        if function_name.eq(needle) {
-
-            // calculate the RVA of the function address
-            let ordinal = unsafe { *ordinals.offset(i.try_into().unwrap()) as usize };
-            let fn_rva = unsafe { *functions.add(ordinal) as usize };
-            // actual memory address of the function address
-            let fn_addr = unsafe { dll_base.add(fn_rva) } as *const c_void;
-
-            let result = ExportResolver {
-                module: dll_name,
-                base_address: dll_base as usize,
-                function: needle,
-                address: fn_addr as usize,
-            };
-
-            return Some(result);
-        }
-    }
-
-    None
-}
-
-/// Read memory of any type
-unsafe fn read_memory<T>(address: *const T) -> T {
-    core::ptr::read(address)
-}
-
-fn wide_to_str<'a>(wide: &[u16], buffer: &'a mut [u8]) -> Option<&'a str> {
-    let mut len = 0;
-
-    for &w in wide {
-        // convert each u16 character to u8 and store in the byte array
-        if w == 0 { break; } // stop at null terminator
-        if w > 0xFF {
-            return None; // non-ASCII characters are not handled here
-        }
-        if len >= buffer.len() {
-            return None; // buffer overflow protection
-        }
-        buffer[len] = w as u8;
-        len += 1;
-    }
-
-    // Convert the byte array to &str
-    match from_utf8_mut(&mut buffer[..len]) {
-        Ok(s) => Some(s),
-        Err(_) => None,
+        // matched
+        true
     }
 }
 
-fn strip_null_terminator(bytes: &[u8]) -> &[u8] {
-    if let Some(pos) = bytes.iter().position(|&x| x == 0) {
-        &bytes[..pos]
-    } else {
-        bytes
-    }
+// types
+pub type USHORT = u16;
+pub type PWCH = *mut u16;
+pub type BYTE = u8;
+pub type BOOL = BYTE;
+pub type HANDLE = *mut c_void;
+pub type PVOID = *mut c_void;
+pub type ULONG = u32;
+pub enum c_void {}
+pub type LPSTR = *mut i8;
+pub type LPCSTR = *const i8;
+pub type DWORD = u32;
+pub type WORD = u16;
+pub type LONG = u32;
+pub type ULONGLONG = u64;
+
+#[repr(C)]
+pub struct PEB {
+
+    pub Reserved1: [u8; 2],
+    pub BeingDebugged: u8,
+    pub Reserved2: [u8; 1],
+    pub Reserved3: [*mut c_void; 2],
+    pub Ldr: *mut PEB_LDR_DATA,
+    pub ProcessParameters: *mut RTL_USER_PROCESS_PARAMETERS,
+    pub Reserved4: [*mut c_void; 3],
 }
 
-#[repr(C, packed(2))]
+#[repr(C)]
+struct PEB_LDR_DATA {
+    Length: ULONG,
+    Initialized: BOOL,
+    SsHandle: HANDLE,
+    InLoadOrderModuleList: LIST_ENTRY,
+    // ...
+}
+
+#[repr(C)]
+struct RTL_USER_PROCESS_PARAMETERS {
+    MaximumLength: ULONG,
+    Length: ULONG,
+    Flags: ULONG,
+    DebugFlags: ULONG,
+    ConsoleHandle: HANDLE,
+    ConsoleFlags: ULONG,
+    StandardInput: HANDLE,
+    StandardOutput: HANDLE,
+    StandardError: HANDLE,
+}
+
+#[repr(C)]
+struct LIST_ENTRY {
+    pub Flink: *mut LIST_ENTRY,
+    pub Blink: *mut LIST_ENTRY,
+}
+
+#[repr(C)] 
+struct LDR_DATA_TABLE_ENTRY {
+    InLoadOrderModuleList: LIST_ENTRY,
+    InMemoryOrderModuleList: LIST_ENTRY,
+    InInitializationOrderModuleList: LIST_ENTRY,
+    BaseAddress: PVOID,
+    EntryPoint: PVOID,
+    SizeOfImage: ULONG,
+    FullDllName: UNICODE_STRING,
+    BaseDllName: UNICODE_STRING,
+}
+
+#[repr(C)] 
+struct UNICODE_STRING {
+    pub Length: USHORT,
+    pub MaximumLength: USHORT,
+    pub Buffer: PWCH,
+}
+
+#[repr(C)]
 pub struct IMAGE_DOS_HEADER {
-    pub e_magic: u16,
-    pub e_cblp: u16,
-    pub e_cp: u16,
-    pub e_crlc: u16,
-    pub e_cparhdr: u16,
-    pub e_minalloc: u16,
-    pub e_maxalloc: u16,
-    pub e_ss: u16,
-    pub e_sp: u16,
-    pub e_csum: u16,
-    pub e_ip: u16,
-    pub e_cs: u16,
-    pub e_lfarlc: u16,
-    pub e_ovno: u16,
-    pub e_res: [u16; 4],
-    pub e_oemid: u16,
-    pub e_oeminfo: u16,
-    pub e_res2: [u16; 10],
-    pub e_lfanew: i32,
+    pub e_magic: WORD,
+    pub e_cblp: WORD,
+    pub e_cp: WORD,
+    pub e_crlc: WORD,
+    pub e_cparhdr: WORD,
+    pub e_minalloc: WORD,
+    pub e_maxalloc: WORD,
+    pub e_ss: WORD,
+    pub e_sp: WORD,
+    pub e_csum: WORD,
+    pub e_ip: WORD,
+    pub e_cs: WORD,
+    pub e_lfarlc: WORD,
+    pub e_ovno: WORD,
+    pub e_res: [WORD; 4],
+    pub e_oemid: WORD,
+    pub e_oeminfo: WORD,
+    pub e_res2: [WORD; 10],
+    pub e_lfanew: LONG,
 }
 
-pub const IMAGE_DOS_SIGNATURE: u16 = 23117u16;
+pub const IMAGE_DOS_SIGNATURE: WORD = 0x5A4D;
 
 #[repr(C)]
 pub struct IMAGE_NT_HEADERS64 {
-    pub Signature: u32,
+    pub Signature: DWORD,
     pub FileHeader: IMAGE_FILE_HEADER,
     pub OptionalHeader: IMAGE_OPTIONAL_HEADER64,
 }
 
 #[repr(C)]
 pub struct IMAGE_FILE_HEADER {
-    pub Machine: IMAGE_FILE_MACHINE,
-    pub NumberOfSections: u16,
-    pub TimeDateStamp: u32,
-    pub PointerToSymbolTable: u32,
-    pub NumberOfSymbols: u32,
-    pub SizeOfOptionalHeader: u16,
-    pub Characteristics: IMAGE_FILE_CHARACTERISTICS,
+    pub Machine: WORD,
+    pub NumberOfSections: WORD,
+    pub TimeDateStamp: DWORD,
+    pub PointerToSymbolTable: DWORD,
+    pub NumberOfSymbols: DWORD,
+    pub SizeOfOptionalHeader: WORD,
+    pub Characteristics: WORD,
 }
 
-#[repr(C, packed(4))]
+#[repr(C)]
 pub struct IMAGE_OPTIONAL_HEADER64 {
-
-    pub Magic: IMAGE_OPTIONAL_HEADER_MAGIC,
-    pub MajorLinkerVersion: u8,
-    pub MinorLinkerVersion: u8,
-    pub SizeOfCode: u32,
-    pub SizeOfInitializedData: u32,
-    pub SizeOfUninitializedData: u32,
-    pub AddressOfEntryPoint: u32,
-    pub BaseOfCode: u32,
-    pub ImageBase: u64,
-    pub SectionAlignment: u32,
-    pub FileAlignment: u32,
-    pub MajorOperatingSystemVersion: u16,
-    pub MinorOperatingSystemVersion: u16,
-    pub MajorImageVersion: u16,
-    pub MinorImageVersion: u16,
-    pub MajorSubsystemVersion: u16,
-    pub MinorSubsystemVersion: u16,
-    pub Win32VersionValue: u32,
-    pub SizeOfImage: u32,
-    pub SizeOfHeaders: u32,
-    pub CheckSum: u32,
-    pub Subsystem: IMAGE_SUBSYSTEM,
-    pub DllCharacteristics: IMAGE_DLL_CHARACTERISTICS,
-    pub SizeOfStackReserve: u64,
-    pub SizeOfStackCommit: u64,
-    pub SizeOfHeapReserve: u64,
-    pub SizeOfHeapCommit: u64,
-    pub LoaderFlags: u32,
-    pub NumberOfRvaAndSizes: u32,
+    pub Magic: WORD,
+    pub MajorLinkerVersion: BYTE,
+    pub MinorLinkerVersion: BYTE,
+    pub SizeOfCode: DWORD,
+    pub SizeOfInitializedData: DWORD,
+    pub SizeOfUninitializedData: DWORD,
+    pub AddressOfEntryPoint: DWORD,
+    pub BaseOfCode: DWORD,
+    pub ImageBase: ULONGLONG,
+    pub SectionAlignment: DWORD,
+    pub FileAlignment: DWORD,
+    pub MajorOperatingSystemVersion: WORD,
+    pub MinorOperatingSystemVersion: WORD,
+    pub MajorImageVersion: WORD,
+    pub MinorImageVersion: WORD,
+    pub MajorSubsystemVersion: WORD,
+    pub MinorSubsystemVersion: WORD,
+    pub Win32VersionValue: DWORD,
+    pub SizeOfImage: DWORD,
+    pub SizeOfHeaders: DWORD,
+    pub CheckSum: DWORD,
+    pub Subsystem: WORD,
+    pub DllCharacteristics: WORD,
+    pub SizeOfStackReserve: ULONGLONG,
+    pub SizeOfStackCommit: ULONGLONG,
+    pub SizeOfHeapReserve: ULONGLONG,
+    pub SizeOfHeapCommit: ULONGLONG,
+    pub LoaderFlags: DWORD,
+    pub NumberOfRvaAndSizes: DWORD,
     pub DataDirectory: [IMAGE_DATA_DIRECTORY; 16],
 }
 
-#[repr(transparent)]
-pub struct IMAGE_FILE_MACHINE(pub u16);
-
-#[repr(transparent)]
-pub struct IMAGE_FILE_CHARACTERISTICS(pub u16);
-
-#[repr(transparent)]
-pub struct IMAGE_OPTIONAL_HEADER_MAGIC(pub u16);
-
-#[repr(transparent)]
-pub struct IMAGE_SUBSYSTEM(pub u16);
-
-#[repr(transparent)]
-pub struct IMAGE_DLL_CHARACTERISTICS(pub u16);
-
 #[repr(C)]
 pub struct IMAGE_DATA_DIRECTORY {
-    pub VirtualAddress: u32,
-    pub Size: u32,
+    pub VirtualAddress: DWORD,
+    pub Size: DWORD,
 }
-
-pub const IMAGE_NT_SIGNATURE: u32 = 17744u32;
 
 #[repr(C)]
 pub struct IMAGE_EXPORT_DIRECTORY {
-    pub Characteristics: u32,
-    pub TimeDateStamp: u32,
-    pub MajorVersion: u16,
-    pub MinorVersion: u16,
-    pub Name: u32,
-    pub Base: u32,
-    pub NumberOfFunctions: u32,
-    pub NumberOfNames: u32,
-    pub AddressOfFunctions: u32,
-    pub AddressOfNames: u32,
-    pub AddressOfNameOrdinals: u32,
+    pub Characteristics: DWORD,
+    pub TimeDateStamp: DWORD,
+    pub MajorVersion: WORD,
+    pub MinorVersion: WORD,
+    pub Name: DWORD,
+    pub Base: DWORD,
+    pub NumberOfFunctions: DWORD,
+    pub NumberOfNames: DWORD,
+    pub AddressOfFunctions: DWORD,
+    pub AddressOfNames: DWORD,
+    pub AddressOfNameOrdinals: DWORD,
 }
